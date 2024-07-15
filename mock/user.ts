2dfdbd1349ebe11288861542a567fa55d7d54962
fanfanxiaozhu <897937048@qq.com>
2024-07-15 09:37:09 +0800

    mock接口

---
+++ mock/user.ts
@@ -0,0 +1,74 @@
+function createUserList() {
+  return [
+    {
+      userId: 1,
+      avatar: 'https://joeschmoe.io/api/v1/random',
+      username: 'admin',
+      password: '123456',
+      desc: '平台管理员',
+      roles: ['平台管理员'],
+      buttons: ['cuser.detail'],
+      routes: ['home'],
+      token: 'admin-token'
+    },
+    {
+      userId: 2,
+      avatar: 'https://joeschmoe.io/api/v1/random',
+      username: 'system',
+      password: '123456',
+      desc: '系统管理员',
+      roles: ['系统管理员'],
+      buttons: ['cuser.detail', 'cuser.edit', 'cuser.delete'],
+      routes: ['home'],
+      token: 'syatem-token'
+    }
+  ]
+}
+
+import { MockMethod } from 'vite-plugin-mock'
+export default [
+  {
+    url: '/api/user/login',
+    method: 'post',
+    response: ({ body }) => {
+      const { username, password } = body
+      const checkUser = createUserList().find(
+        (item) => item.username === username && password === item.password
+      )
+      if (!checkUser) {
+        return {
+          code: 201,
+          data: { message: '账号或密码不正确' }
+        }
+      }
+      const { token } = checkUser
+      return {
+        code: 0,
+        data: {
+          token
+        }
+      }
+    }
+  },
+  {
+    url: '/api/user/info',
+    method: 'get',
+    timeout: 2000,
+    response: (request) => {
+      const token = request.headers.token
+      const checkUser = createUserList().find((item) => item.token === token)
+      if (!checkUser) {
+        return {
+          code: 201,
+          data: { message: '账号或密码不正确' }
+        }
+      }
+      return {
+        code: 0,
+        data: {
+          checkUser
+        }
+      }
+    }
+  }
+] as MockMethod[]
--- vite.config.ts
+++ vite.config.ts
@@ -1,29 +1,35 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
 import { createSvgIconsPlugin } from 'vite-plugin-svg-icons'
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    createSvgIconsPlugin({
-      iconDirs: [path.resolve(process.cwd(), 'src/assets/icons')],
-      symbolId: 'icon-[dir]-[name]'
-    })
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  },
-  css: {
-    preprocessorOptions: {
-      scss: {
-        javascriptEnabled: true,
-        additionalData: '@import "./src/styles/variable.scss";',
-      },
+import { viteMockServe } from 'vite-plugin-mock'
+
+export default defineConfig(({ command }) => {
+  return {
+    plugins: [
+      vue(),
+      createSvgIconsPlugin({
+        iconDirs: [path.resolve(process.cwd(), 'src/assets/icons')],
+        symbolId: 'icon-[dir]-[name]'
+      }),
+      viteMockServe({
+        mockPath: 'mock',
+        enable: command === 'serve'
+      })
+    ],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      }
     },
-  },
+    css: {
+      preprocessorOptions: {
+        scss: {
+          javascriptEnabled: true,
+          additionalData: '@import "./src/styles/variable.scss";'
+        }
+      }
+    }
+  }
 })
