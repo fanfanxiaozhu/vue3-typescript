34e46b6919b77767f1f085e972e63a8b8ff26166
fanfanxiaozhu <897937048@qq.com>
2024-07-16 11:29:22 +0800

    登录页对接接口

--- mock/user.ts
+++ mock/user.ts
@@ -43,7 +43,7 @@ export default [
       }
       const { token } = checkUser
       return {
-        code: 0,
+        code: 200,
         data: {
           token
         }
@@ -64,7 +64,7 @@ export default [
         }
       }
       return {
-        code: 0,
+        code: 200,
         data: {
           checkUser
         }
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -3,18 +3,18 @@
         <el-row>
             <el-col :span="12" :xs="0"></el-col>
             <el-col :span="12" :xs="24">
-                <el-form class="form" :model="form" label-width="auto">
+                <el-form class="form" :model="form" :rules="rules" ref="formRef">
                     <h1>Hello</h1>
                     <h3>欢迎来到二哈甄选运营平台</h3>
-                    <el-form-item>
+                    <el-form-item prop="username">
                         <el-input v-model="form.username" :prefix-icon="User" />
                     </el-form-item>
-                    <el-form-item>
+                    <el-form-item prop="password">
                         <el-input type="password" v-model="form.password" :prefix-icon="Lock" show-password>
                         </el-input>
                     </el-form-item>
                     <el-form-item>
-                        <el-button class="w-100" type="primary" @click="onSubmit">登录</el-button>
+                        <el-button class="w-100" type="primary" :loading="loading" @click="onSubmit(formRef)">登录</el-button>
                     </el-form-item>
                 </el-form>
             </el-col>
@@ -24,15 +24,59 @@
 </template>
 
 <script setup lang='ts'>
-import { reactive } from 'vue'
+import { ref, reactive } from 'vue'
 import { User, Lock } from '@element-plus/icons-vue'
+import type { FormInstance, FormRules } from 'element-plus'
+import { ElNotification } from 'element-plus'
+import { reqLogin } from '@/api/user'
+import { useRouter } from 'vue-router'
+const router = useRouter()
+
 const form = reactive({
     username: 'admin',
     password: '123456',
 })
 
-const onSubmit = () => {
-    console.log('submit!')
+const rules = reactive<FormRules>({
+    username: [
+        { required: true, message: '请输入用户名', trigger: 'blur' },
+    ],
+    password: [
+        { required: true, message: '请输入密码', trigger: 'blur' },
+    ],
+})
+
+const formRef = ref<FormInstance>()
+const onSubmit = (formRef: any) => {
+    if (!formRef) return;
+    formRef.validate((valid: boolean) => {
+        if (valid) {
+            login();
+        } else {
+            return false
+        }
+    })
+}
+
+const loading = ref(false)
+const login = () => {
+    loading.value = true
+    reqLogin(form).then((res) => {
+        loading.value = false
+        if (res.code === 200) {
+            localStorage.setItem('TOKEN', res.data.token as string)
+            router.push({ name: 'home' })
+            ElNotification({
+                type: 'success',
+                message: `Hi,${form.username},欢迎回来!`,
+            })
+        } else {
+            ElNotification({
+                type: 'error',
+                message: res.data.message,
+            })
+        }
+    })
 }
 
 </script>
