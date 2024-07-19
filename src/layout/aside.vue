51304faa78260fd638dbe51fe3e40913a82a0a9c
fanfanxiaozhu <897937048@qq.com>
2024-07-19 16:22:40 +0800

    左侧导航加滚动条

--- src/layout/aside.vue
+++ src/layout/aside.vue
@@ -4,7 +4,9 @@
       <img :src="config.logo" alt="logo" width="46">
       <span>{{ config.title }}</span>
     </div>
-    <MenuList :menu="menuList"></MenuList>
+    <el-scrollbar>
+      <MenuList :menu="menuList"></MenuList>
+    </el-scrollbar>
   </el-menu>
 </template>
 
@@ -21,6 +23,10 @@ const activeMenu = router.path
 </script>
 
 <style lang="scss" scoped>
+.el-scrollbar {
+  height: calc(100vh - #{$height-logo});
+}
+
 .logo {
   height: $height-logo;
   display: flex;
